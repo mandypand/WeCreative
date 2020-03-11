2f9ae720af4c6ccf6a5a3c635602f8c81f62aa40
rodde93 <rodrigo.s-wdk@outlook.com>
2020-03-11 17:38:06 +0100

    importDataPost.js post.db added post code to app.js

--- app.js
+++ app.js
@@ -1,41 +1,67 @@
 const express = require('express')
 const Datastore = require('nedb-promise')
 const cors = require('cors')
-const users = new Datastore ({filename: 'users.db', autoload: true})
+const users = new Datastore({ filename: 'users.db', autoload: true })
+const post = new Datastore({ filename: 'post.db', autoload: true })
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
     const responsiveJSON = await users.find({})
-    res.json({'responsiveJSON': responsiveJSON})
-}) 
+    res.json({ 'responsiveJSON': responsiveJSON })
+})
 
-app.get('/users/:id', async(req, res) => {
-    const documents = await users.findOne({_id: req.params.id})
+app.get('/users/:id', async (req, res) => {
+    const documents = await users.findOne({ _id: req.params.id })
     res.json(documents)
 })
 
-app.post('/users', async(req,res) => {
-    let user = await users.findOne({email: req.body.email})
+app.post('/users', async (req, res) => {
+    let user = await users.findOne({ email: req.body.email })
     let emailSign = req.body.email
 
-if(!user){
-    let newUser = {
-        name: req.body.name,
-        surname: req.body.surname,
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password 
-    }
-    const documents = await users.insert(newUser);
-    res.json(documents)
+    if (!user) {
+        let newUser = {
+            name: req.body.name,
+            surname: req.body.surname,
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        }
+        const documents = await users.insert(newUser);
+        res.json(documents)
 
-} else {
-    console.log("error")
-}
-     
+    } else {
+        console.log("error")
+    }
+
 })
 
+
+//--Rodrigo post--//
+app.post("/add", (req, res) => {
+    const newNote = {
+        title: req.body.title,
+        content: req.body.content,
+        owner: req.session.user
+    };
+    note.insert(newNote, function (err, doc) {
+        res.redirect("/");
+    });
+});
+
+app.post("/delete/:noteId", (req, res) => {
+    notes.remove({ _id: req.params.noteId }, function (err, doc) {
+        res.redirect("/");
+    });
+});
+
+app.post("/update/:noteId", (req, res) => {
+    notes.update({ _id: req.params.noteId }, function (err, doc) {
+        res.redirect("/");
+    });
+});
+//--post-end--//
 app.listen(8070, () => console.log('Server started'))
--- importData.js
+++ importData.js
@@ -1,7 +1,8 @@
 const Datastore = require('nedb-promise')
-const users = new Datastore({ filename: 'users.db', autoload: true})
-let userList = require ('./users.json')
+const users = new Datastore({ filename: 'users.db', autoload: true })
+let userList = require('./users.json')
 
-users.remove({}).then(() =>{
+
+users.remove({}).then(() => {
     users.insert(userList.results);
 })
