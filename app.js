51de3cc822bdc02b04a9038dd9af25d93b42146a
Amanda Jakobsson <Amanda@Amandas-MacBook-Air.local>
2020-03-16 13:36:46 +0100

    login

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ app.get('/users/:id', async (req, res) => {
     res.json(documents)
 })
 
+// LOGIN
+// app.post('/login', async (req, res) =>{
+//     const user =  await users.findOne({email: req.body.email})
+//     if(user && user.password == req.body.password){
+//         res.status(200)
+//         res.send('sucess')
+//     } else {
+//         res.status(404)
+//         res.send({error: 'error'})
+//     }
+// })
+
+
 app.post('/users', async (req, res) => {
     let user = await users.findOne({ email: req.body.email })
     let emailSign = req.body.email
