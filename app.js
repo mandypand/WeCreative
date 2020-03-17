38fbee14c1dff5ae6610c56e967c87e0a4bb620e
rodde93 <rodrigo.s-wdk@outlook.com>
2020-03-17 18:48:22 +0100

    commited latets uppdatet to app.js

--- app.js
+++ app.js
@@ -8,21 +8,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
     const responsiveJSON = await users.find({})
     res.json({ 'responsiveJSON': responsiveJSON })
 })
 
-app.get('/users/:id', async(req, res) => {
+app.get('/users/:id', async (req, res) => {
     const documents = await users.findOne({ _id: req.params.id })
     res.json(documents)
 })
-
-
-
-
-
-
 
 // LOGIN
 // app.post('/login', async (req, res) =>{
@@ -37,7 +31,7 @@ app.get('/users/:id', async(req, res) => {
 // })
 
 
-app.post('/users', async(req, res) => {
+app.post('/users', async (req, res) => {
     let user = await users.findOne({ email: req.body.email })
     let emailSign = req.body.email
 
@@ -58,6 +52,8 @@ app.post('/users', async(req, res) => {
 
 })
 
+///---------From Merge---------///
+/*
 app.get('/post', async (req, res) => {
     const responsiveJSON = await post.find({})
     res.json({ 'responsiveJSON': responsiveJSON })
@@ -84,68 +80,17 @@ app.patch('/post/:id', async (req, res) => {
     );
     res.json(documents);
 });
-
-
-/*
-app.patch('/users/:id', (req, res) => {
-    post.edit({ _id: req.params.id }).update(req.body), function (err, doc) {
-        res.json(edit)
-    }
-})
 */
+///---------From Merge---------///
 
-/*
-app.get('/post/:id', async (req, res) => {
-    const responsiveJSON = await post.find({ _id: parseInt(req.params.id) })
-    if (responsiveJSON.length > 0) {
-        res.json({ 'responsiveJSON': responsiveJSON })
-    } else {
-        res.status(404);
-        res.send({ error: 'Not Found' });
-    }
-})
-/*
-
-//--Rodrigo post--//
-
-<<<<<<< HEAD
-/*
-app.post("/post/add", (req, res) => {
-=======
-app.get("/post/add", (req, res) => {
->>>>>>> d6a1d40bf9ce39264578c43791a2d6d19b563da6
-    const newNote = {
-        title: req.body.title,
-        content: req.body.content,
-        owner: req.session.user
-    };
-    note.insert(newNote, function(err, doc) {
-        res.redirect("/");
-    });
-});
-
-app.post("/post/delete/:noteId", (req, res) => {
-    notes.remove({ _id: req.params.noteId }, function(err, doc) {
-        res.redirect("/");
-    });
-});
-
-app.post("/post/update/:noteId", (req, res) => {
-    notes.update({ _id: req.params.noteId }, function(err, doc) {
-        res.redirect("/");
-    });
-});
-*/
-
-
-app.post('/delete/:post_Id', async(req, res) => {
+app.post('/delete/:post_Id', async (req, res) => {
     post.remove({ _id: req.params.post_Id }, {
         function(err, doc) {
             res.redirect('/')
         }
     })
 })
-app.post('/delete/:post_Id', async(req, res) => {
+app.post('/delete/:post_Id', async (req, res) => {
     post.remove({ _id: req.params.post_Id }, {
         function(err, doc) {
             res.redirect('/')
@@ -153,24 +98,37 @@ app.post('/delete/:post_Id', async(req, res) => {
     })
 })
 
-app.get('/post', async(req, res) => {
+app.get('/post', async (req, res) => {
     const responsiveJSON = await post.find({})
     res.json({ 'responsiveJSON': responsiveJSON })
 })
 
 
-app.get('/post/:id', async(req, res) => {
+app.get('/post/:id', async (req, res) => {
     const documents = await post.findOne({ _id: req.params.id })
     res.json(documents)
 })
 
 
-app.delete('/post/:id', async(req, res) => {
+app.delete('/post/:id', async (req, res) => {
     const documents = await post.remove({ _id: req.params.id })
     res.json(documents)
 })
 
-app.patch('/post/:id', async(req, res) => {
+app.patch('/post/:id', async (req, res) => {
+    const documents = await post.update({ _id: req.params.id }, {
+        $set: {
+            title: req.body.title,
+            type: req.body.type,
+            file: req.body.file,
+            author: req.body.author
+        }
+    })
+    res.json({ "documents": documents })
+})
+
+/*
+app.patch('/post/:id', async (req, res) => {
     const documents = await post.update({ _id: req.params.id }, {
         $set: {
             "post.title": req.body.title,
@@ -181,7 +139,7 @@ app.patch('/post/:id', async(req, res) => {
     })
     res.json(documents)
 })
-
+*/
 
 //--post-end--//
 app.listen(8070, () => console.log('Server started'))
