4d64d79282227f8885a0afe5dd2557cae7b18245
Caroline Olby <carolineolby@Carolines-MacBook-Pro.local>
2020-03-19 09:48:42 +0100

    update

--- public/toggle.js
+++ public/toggle.js
@@ -2,7 +2,6 @@ function hideShow() {
     const edit = document.querySelector(".Toggle__Edit");
         if (edit.style.display === "none") {
             edit.style.display = "block";           
-            edit.querySelector("editText").innerHTML = "New text!";
         } else {
             edit.style.display = "none";
         }
