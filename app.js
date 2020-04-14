80a367fbcafa2bc9abcf5e7ee7981b31528cd2dc
rodde93 <rodrigo.s-wdk@outlook.com>
2020-04-14 13:53:01 +0200

    authentication update

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const Datastore = require('nedb-promise')
 const cors = require('cors')
 const bcrypt = require('bcryptjs')
+
+//routes
+const authentication = require('./routes/authentication.js')
+
 const users = new Datastore({ filename: 'users.db', autoload: true })
 const post = new Datastore({ filename: 'post.db', autoload: true })
 const app = express()
@@ -9,6 +13,7 @@ const app = express()
 app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
+app.use(authentication)
 
 app.get('/users', async (req, res) => {
     const responsiveJSON = await users.find({})
@@ -20,56 +25,7 @@ app.get('/users/:id', async (req, res) => {
     res.json(documents)
 })
 
-app.get('/login', async (req, res) => {
-    const responsiveJSON = await users.find({})
-    res.json({ 'responsiveJSON': responsiveJSON })
-   
-    
-})
-
-app.post('/users', async (req, res) => {
-    let user = await users.findOne({ email: req.body.email })
-
-    if (!user) {
-        const password = req.body.password
-        const hash = await bcrypt.hash(password, 10)
-
-        let newUser = {
-            name: req.body.name,
-            surname: req.body.surname,
-            username: req.body.username,
-            email: req.body.email,
-            password: hash
-        }
-        const documents = await users.insert(newUser);
-        res.json(documents)
-
-    } else {
-        res.status(404)
-        res.send({error: 'error'})
-    }
-
-})
-
-app.post('/login', async (req, res) => {
-    const user = await users.findOne({username: req.body.username}) 
-    const userPassword = req.body.password
-         if(user){
-            const correctPassword = await bcrypt.compare(userPassword, user.password)
-             if(correctPassword) {
-                res.status(200)
-                res.send('sucess')
-             } else {
-                res.status(404)
-                res.send({error: 'error'})
-             }
-         } else {
-             res.status(404)
-             res.send({error: 'error'})
-         }
- })
-
- app.get('/post', async (req, res) => {
+app.get('/post', async (req, res) => {
     const responsiveJSON = await post.find({})
     res.json({ 'responsiveJSON': responsiveJSON })
 })
@@ -81,7 +37,7 @@ app.post('/post', async (req, res) => {
         author: req.body.author
     };
 
-    const documents = await post.insert(newPost) 
+    const documents = await post.insert(newPost)
     res.json(documents)
 })
 
